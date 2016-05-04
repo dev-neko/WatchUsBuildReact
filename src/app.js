68ce227c0c6cde1405478cd33260270f2b0ce5aa
dev-neko <pi3kp85@gmail.com>
2016-05-04 09:52:32 +0900

    Add react-router. Apply the minimum layout

--- src/app.js
+++ src/app.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import jQuery from 'jQuery';
 import CommentBox from './components/comment-box';
+import {Router, Route} from 'react-router';
+import Layout from './layout/layout';
+
+const app = (
+  <Router>
+    <Route path="/" component={Layout} />
+  </Router>
+)
 
 jQuery(function() {
   ReactDOM.render(
-    <CommentBox />,
+    app,
     document.getElementById('comment-box'),
     () => console.timeEnd('react-app')
   );
